d387e7f50a4d8aa2e52f9c7410037584b3674f67
dhrumilparmar <dhrumilparmar27481@gmail.com>
2026-04-20 09:05:36 +0530

    establish connection and call insert and fetch operations

--- src/main/java/com/hibernet/App.java
+++ src/main/java/com/hibernet/App.java
@@ -1,5 +1,13 @@
 package com.hibernet;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,10 +16,49 @@ public class App
 {
     public static void main( String[] args ) {
 
+        //create object to insert data
         Student s1 = new Student();
-        s1.setSid(101);
-        s1.setS_name("Dhrumil");
-        s1.setDomain("CloudOps");
+//        s1.setSid(104);
+//        s1.setS_name("Mann");
+//        s1.setDomain("AI-ML");
+
+        //load the configurstion files
+        Configuration config = new Configuration();
+        //locate the class
+        config.addAnnotatedClass(com.hibernet.Student.class);
+        //locate the config file
+        config.configure("hibernate.cfg.xml");
+
+        //start a session for config file
+        SessionFactory factory = config.buildSessionFactory();
+        //open the session
+        Session session = factory.openSession();
+
+        //open the transcation
+        Transaction tx = session.beginTransaction();
+        // save the data
+//        session.persist(s1);
+//        to fetch the data
+//        Student s2 = session.find(Student.class,106);
+        List<Student> list = session.createQuery("from Student", Student.class)
+                .getResultList();
+
+        for (Student s : list) {
+            System.out.println( s.getS_name() + " " + s.getDomain());
+        }
+
+        Student s = list.get(0);
+        s.setDomain("DevOps");
+
+
+//        Student s2 = session.createQuery();
+//        System.out.println(s2);
+
+        //close the trancations and session and factory
+        tx.commit();
+        session.close();
+        factory.close();
+
     }
 
 
